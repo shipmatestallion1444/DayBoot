1e3047ada991cc3f9fcbda3201f26906483d03dd
globuscoding <97716049+shipmatestallion1444@users.noreply.github.com>
2023-05-22 23:19:20 +0200

    Add a python file for the Wikipedia API

---
+++ wikipedia.py
@@ -0,0 +1,24 @@
+import wikipedia
+
+def get_first_paragraph(title):
+    try:
+        # Fetch the Wikipedia page
+        page = wikipedia.page(title)
+        
+        # Extract the first paragraph
+        first_paragraph = page.content.split('\n')[0]
+        
+        return first_paragraph
+    except wikipedia.exceptions.PageError:
+        return "Page not found"
+    except wikipedia.exceptions.DisambiguationError:
+        return "Disambiguation page found"
+
+# Specify the Wikipedia article title
+article_title = "Python (programming language)"
+
+# Call the function to get the first paragraph
+first_paragraph = get_first_paragraph(article_title)
+
+# Print the result
+print(first_paragraph)
