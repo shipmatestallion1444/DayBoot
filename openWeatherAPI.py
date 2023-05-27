d75b476e8ebd1c86beb7d3d9112fb9a46f37ac81
globuscoding <97716049+shipmatestallion1444@users.noreply.github.com>
2023-05-27 19:34:37 +0200

    Change temperature in weather api from K to C

--- openWeatherAPI.py
+++ openWeatherAPI.py
@@ -1,6 +1,7 @@
 import requests
 import time
 # Coordinates of Kiel: 54.32133 10.13489
+# 54.3217/10.1412
 # OpenWeather API key: d056c9342b30b7eb2802231b3f1d2c54
 
 # API GET request URL with Kiel and the API key
@@ -14,15 +15,17 @@ data = response.json()
 
 # Extract the required information
 temperature = data["main"]["temp"]
-temperaturec = (temperature - 30)/2
+temperaturec = round(temperature - 273.15)
 feels_like_temperature = data["main"]["feels_like"]
-feels_like_temperature_c = (feels_like_temperature - 30)/2
+feels_like_temperature_c = round(feels_like_temperature - 273.15)
 rain_status = data["weather"][0]["description"]
 rain_soon = "It will rain soon!" if "rain" in data else "It'll stay dry - hopefully"
 
 # Output the information
-print(f"Temperature: {temperaturec} C")
-print(f"Feels Like: {feels_like_temperature_c} C")
+print (data)
+print(f"{temperature}")
+print(f"Temperature: {temperaturec} °C")
+print(f"Feels Like: {feels_like_temperature_c} °C")
 print(f"Rain Status: {rain_status}")
 print(f"{rain_soon}")
 
